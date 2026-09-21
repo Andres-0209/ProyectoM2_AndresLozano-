be6c181af958b0d0eef41688ebd4df21d73846c4
Andrés Felipe Lozano Aguilar <anfeloa03@gmail.com>
2026-09-21 09:17:51 -0500

    Actualización db.js y .env

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,25 @@
+// import pkg from 'pg';
+// const { Pool } = pkg;
+
+// const db = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+// });
+
+// export default db;
+
 import pkg from 'pg';
 const { Pool } = pkg;
 
 const db = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 export default db;
-
-
 
 
 
