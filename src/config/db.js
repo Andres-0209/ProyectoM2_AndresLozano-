ef687b9a988e07ddbe108be3c8a4916ab04c0804
Andrés Felipe Lozano Aguilar <anfeloa03@gmail.com>
2026-09-20 19:51:12 -0500

    Verificación de conexión

--- src/config/db.js
+++ src/config/db.js
@@ -1,51 +1,18 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-
-const db = new Pool(
-  process.env.DATABASE_URL
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false }
-      }
-    : {
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME,
-      }
-);
-
-
-const initTables = async () => {
-  const sql = `
-    CREATE TABLE IF NOT EXISTS authors (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL
-    );
-
-    CREATE TABLE IF NOT EXISTS posts (
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(200) NOT NULL,
-        content TEXT NOT NULL,
-        author_id INT REFERENCES authors(id) ON DELETE CASCADE,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-  `;
-  try {
-    await db.query(sql);
-    console.log('Tablas del MiniBlog verificadas y creadas con éxito en PostgreSQL.');
-  } catch (err) {
-    console.error('Error al inicializar las tablas:', err);
-  }
-};
-
-// Ejecuta la creación de tablas al importar el módulo
-initTables();
+const db = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
 
 export default db;
+
+
+
 
 
 
